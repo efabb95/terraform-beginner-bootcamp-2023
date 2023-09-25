0416e97b1592add6c251986d53073232cbc0f19e
Elia Fabbris <elia.fabbris@gmail.com>
2023-09-25 15:00:09 +0000

    #26 refactor s3 bucket into the new terrahouse_aws module

---
+++ modules/terrahouse_aws/variables.tf
@@ -0,0 +1,17 @@
+variable "user_uuid" {
+  description = "The UUID of the user"
+  type        = string
+  validation {
+    condition        = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
+    error_message    = "The user_uuid value is not a valid UUID."
+  }
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9.-]{3,63}$", var.bucket_name))
+    error_message = "Invalid bucket name. The name must be between 3 and 63 characters, contain only letters, numbers, hyphens, and periods, and must not start or end with a hyphen or period."
+  }
+}
