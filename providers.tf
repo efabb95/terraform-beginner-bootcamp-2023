2dfabfd502873d50f40f418f6db7d7306fe37103
Elia Fabbris <39314018+efabb95@users.noreply.github.com>
2023-09-25 17:00:53 +0200

    #26 refactor s3 bucket into the new terrahouse_aws module (#27)

--- providers.tf
+++ providers.tf
@@ -6,15 +6,7 @@ terraform {
   #    name = "terra-house-1"
   #  }
   #}
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.16.2"
-    }
-  }
 }
 provider "aws" {
 }
-provider "random" {
-  # Configuration options
-}
+
