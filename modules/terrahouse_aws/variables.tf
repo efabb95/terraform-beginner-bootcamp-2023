a2d67c340249b84e5e52c56566804be5c262d57a
Elia Fabbris <elia.fabbris@gmail.com>
2023-09-29 07:28:49 +0000

    #32 only upload index.html if a content version has change.

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -33,3 +33,14 @@ variable "error_html_filepath" {
     error_message = "File error.html not exists"
   }
 }
+
+variable "content_version" {
+  description = "Content version. Integer starting at 1"
+  type = number
+
+  validation {
+    condition = var.content_version > 0 && floor(var.content_version) == var.content_version
+    error_message = "The content_version must be a positive integer starting at 1"
+  }
+  
+}
