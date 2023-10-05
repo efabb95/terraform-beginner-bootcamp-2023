0668a64e72d4cab9938826b393be44868155064c
Elia Fabbris <elia.fabbris@gmail.com>
2023-10-05 12:03:21 +0000

    #52 test out terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -23,21 +23,21 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="c4b4de80-f521-467a-a306-4de5aaf98d55" 
-  token="6d265b29-ee2d-4927-a1e4-3cb3463d8353"
+  endpoint = var.terratowns_endpoint
+  user_uuid=var.teacherseat_user_uuid
+  token=var.terratowns_access_token
   
 }
 
-//module "terrahouse_aws" {
-//  source = "./modules/terrahouse_aws"
-//  user_uuid = var.user_uuid
-//  bucket_name = var.bucket_name
-//  index_html_filepath = var.index_html_filepath
-//  error_html_filepath = var.error_html_filepath
-//  content_version = var.content_version
-//  assets_path = var.assets_path
-//}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 
 resource "terratowns_home" "home" {
@@ -48,8 +48,8 @@ and the second-most populous city proper in Italy after Rome.
 The city proper has a population of about 1.4 million,
 while its metropolitan city has 3.26 million inhabitants.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fdq3gz.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
