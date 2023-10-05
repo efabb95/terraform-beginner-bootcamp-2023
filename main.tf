06dd91d46a7e972e8e683d8786671aa0df793bd2
Elia Fabbris <39314018+efabb95@users.noreply.github.com>
2023-10-05 14:04:11 +0200

    50 terratown test (#51)
    * Update main.tf
    
    * #52 test out terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -23,21 +23,21 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid=var.teacherseat_user_uuid
+  token=var.terratowns_access_token
   
 }
 
-//module "terrahouse_aws" {
-//  source = "./modules/terrahouse_aws"
-//  user_uuid = var.user_uuid
-//  bucket_name = var.bucket_name
-//  index_html_filepath = var.index_html_filepath
-//  error_html_filepath = var.error_html_filepath
-//  content_version = var.content_version
-//  assets_path = var.assets_path
-//}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 
 resource "terratowns_home" "home" {
@@ -48,8 +48,8 @@ and the second-most populous city proper in Italy after Rome.
 The city proper has a population of about 1.4 million,
 while its metropolitan city has 3.26 million inhabitants.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fdq3gz.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
